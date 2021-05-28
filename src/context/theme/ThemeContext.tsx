1fcedcd9d63d41e2b0ec225ab7572f0f2badbfcf
Luis Garcia <lalejandro.1988@gmail.com>
2021-05-27 22:03:32 -0600

    Dark-light theme change update#2 system-based

--- src/context/theme/ThemeContext.tsx
+++ src/context/theme/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useReducer } from 'react';
-import { themeReducer, ThemeState, lightTheme } from './themeReducer';
+import React, { createContext, useEffect, useReducer } from 'react';
+import { Appearance, AppState, useColorScheme } from 'react-native';
+import { themeReducer, ThemeState, lightTheme, darkTheme } from './themeReducer';
 
 // Props ThemeContext
 interface ThemeContextProps {
@@ -12,7 +13,36 @@ export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
 
-    const [theme, dispatch] = useReducer(themeReducer, lightTheme ) // TODO: 
+    // const colorScheme = useColorScheme();
+
+    const [theme, dispatch] = useReducer(themeReducer, (Appearance.getColorScheme() === 'dark') ? darkTheme: lightTheme ) // TODO: 
+
+    useEffect(() => {
+
+        /**
+         * Usar AppState y Appearance.getColorScheme para que el cambio
+         * funcione en IOS y Android.
+         */
+        AppState.addEventListener('change', (status) => {
+
+            if ( status === 'active' ) {
+                ( Appearance.getColorScheme() === 'light' )
+                    ? setLightTheme()
+                    : setDarkTheme();
+            }
+
+        });
+
+    }, []);
+
+    // Solo funciona en IOS
+    // useEffect(() => {
+        
+    //     (colorScheme === 'light')
+    //         ? setLightTheme()
+    //         : setDarkTheme();
+
+    // }, [colorScheme]);
 
     const setDarkTheme = () => {
         dispatch({ type: 'set_dark_theme' });
